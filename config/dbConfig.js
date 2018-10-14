b374b70a7dd58d2ae159c242d428bd706c3f869f
curiositypaths <curiositypaths@users.noreply.github.com>
2018-10-13 22:52:08 -0400

    organize-file-structure

--- config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-const dbConfig = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: "postgres"
-};
-
-module.exports = dbConfig;

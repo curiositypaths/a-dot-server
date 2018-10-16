03f3d8c8057a8b51a3881c27cbb7e4ee1fb59a0d
curiositypaths <curiositypaths@users.noreply.github.com>
2018-10-15 22:50:35 -0400

    Correct weback HMR callback function

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
 const { createServer } = require("http");
-const app = require("./app");
+let app = require("./app");
 
 const port = 4000;
 const server = createServer(app);
@@ -8,7 +8,8 @@ let currentApp = app;
 server.listen(port);
 
 if (module.hot) {
-  module.hot.accept(["./app"], () => {
+  module.hot.accept("./app", () => {
+    app = require("./app");
     server.removeListener("request", currentApp);
     server.on("request", app);
     currentApp = app;

bcd8bcaf2a7fb5563e0dadc10c849f99c6f46bbc
curiositypaths <curiositypaths@users.noreply.github.com>
2018-10-22 21:18:21 -0400

    Improve error messages

--- app/helpers/validators.js
+++ app/helpers/validators.js
@@ -17,13 +17,22 @@ const validateParams = (params, schema) => {
   };
 };
 
+// The two helper methods below are are used to transform validations errors
+// from: "\"firstName\" length must be at least 2 characters long"
+// to:   "length must be at least 2 characters long"
+// this should allow a simpler recycling of the error messages.
+// firstName does not mean anything to the user
+const findValuesBetweenDoubleQuotes = new RegExp(/(["])(\\?.)*?\1/);
+const cleanMessage = msg =>
+  msg.replace(findValuesBetweenDoubleQuotes, "").trim();
+
 const formatSchemaValidationErrors = ({ details: errorsDetails }) => {
   return errorsDetails.reduce((msg, validationError) => {
     const paramName = validationError.path[0];
     if (msg[paramName]) {
-      msg[paramName].push(validationError.message);
+      msg[paramName].push(cleanMessage(validationError.message));
     } else {
-      msg[paramName] = [validationError.message];
+      msg[paramName] = [cleanMessage(validationError.message)];
     }
     return msg;
   }, {});

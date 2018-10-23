300fb6c73fdcd1054778bba462dbf6383a861be0
curiositypaths <curiositypaths@users.noreply.github.com>
2018-10-22 20:11:59 -0400

    Improve cors configuration

--- app/app.js
+++ app/app.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 const { apiRouterPrefix, apiRouter } = require("./routes/api");
 
 const app = express();
+
+const corsOptions = {
+  origin: process.env.WEB_CLIENT_ADDRESS,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  "Access-Control-Max-Age": 600,
+  optionsSuccessStatus: 200 // some legacy browsers choke on 204
+};
+
 app.use(cors());
 app.use(apiRouterPrefix, apiRouter);
 
